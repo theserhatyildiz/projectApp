48d311aef76a8918cc0fe48a8310f23274bf00f8
Serhat Yildiz <yildizserhat@yahoo.com>
2024-06-12 16:53:03 +0300

    PWA changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,56 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',  
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],  
+
+      manifest: {  
+        name: 'Galwin',  
+              short_name: 'Galwin',  
+              description: 'App to track your nutrition and weight',  
+              theme_color: '#ffffff',  
+              start_url: '/',  
+              icons: [  
+              {  
+                src: 'pwa-192x192.png',  
+                sizes: '192x192',  
+                type: 'image/png',  
+              },  
+              {  
+                src: 'pwa-512x512.png',  
+                sizes: '512x512',  
+                type: 'image/png',  
+              },  
+              {  
+                src: 'pwa-512x512.png',  
+                sizes: '512x512',  
+                type: 'image/png',  
+                purpose: 'any maskable',  
+              },  
+              
+                    ],  
+              },  
+              workbox: {
+                globPatterns: ['**/*.{js,css,html,png,jpg,svg}'],
+                runtimeCaching: [
+                  {
+                    urlPattern: ({ url }) => url.pathname.startsWith('/'),
+                    handler: 'NetworkFirst',
+                    options: {
+                      cacheName: 'api-cache',
+                      cacheableResponse: {
+                        statuses: [0, 200],
+                      },
+                    },
+                  },
+                ],
+              },
+            }),  
+          ],  
+        });
