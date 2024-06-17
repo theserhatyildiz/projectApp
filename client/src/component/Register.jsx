f9cd0d936810a24d15cff5830c1672ba433ef1be
Serhat Yildiz <yildizserhat@yahoo.com>
2024-06-17 14:51:22 +0300

    register page and server updated. confirm password and the email existence check add

--- client/src/component/Register.jsx
+++ client/src/component/Register.jsx
@@ -10,6 +10,7 @@ export default function Register()
         name:"",
         email:"",
         password:"",
+        passwordConfirm: "",
         age:""
     })
 
@@ -32,36 +33,45 @@ export default function Register()
         event.preventDefault();
         console.log(userDetails);
 
+    // Check if passwords match
+    if (userDetails.password !== userDetails.passwordConfirm) {
+        setMessage({ type: "error", text: "Şifreler uyuşmuyor!" });
+        return;
+    }
+
+
     // ------------------Sending the data to API------------------
 
-    fetch("https://galwinapp-7861c5aaed27.herokuapp.com/register",{
-        method:"POST",
-        body:JSON.stringify(userDetails),
-        headers:{
-            "Content-type":"application/json"
-        }
-    })
-    .then((response)=>response.json())
-    .then((data)=>{
-        
-        setMessage({type:"success", text:data.message})
-
-        setUserDetails({
-            name:"",
-            email:"",
-            password:"",
-            age:"",
+    fetch("http://localhost:8000/register", {
+            method: "POST",
+            body: JSON.stringify(userDetails),
+            headers: {
+                "Content-type": "application/json"
+            }
         })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.message === 'Email zaten kayıtlı!') {
+                setMessage({ type: "error", text: data.message });
+            } else {
+                setMessage({ type: "success", text: data.message });
 
-        setTimeout(() => {
-            setMessage({type:"invisible-msg", text:"Dummy Msg"})
-        }, 3000);
+                setUserDetails({
+                    name: "",
+                    email: "",
+                    password: "",
+                    passwordConfirm: "",
+                    age: ""
+                });
 
-
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+                setTimeout(() => {
+                    setMessage({ type: "invisible-msg", text: "Dummy Msg" });
+                }, 3000);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
     }
 
 
@@ -75,6 +85,7 @@ export default function Register()
                 <input className="inp" type="text" onChange={handleInput} placeholder="İsim Girin" name="name" value={userDetails.name} required/>
                 <input className="inp" type="email" onChange={handleInput} placeholder="Email Girin" name="email" value={userDetails.email} required/>
                 <input className="inp" type="password" minLength={8} onChange={handleInput} placeholder="Şifre Girin" name="password" value={userDetails.password} required/>
+                <input className="inp" type="password" minLength={8} onChange={handleInput} placeholder="Şifreyi Onayla" name="passwordConfirm" value={userDetails.passwordConfirm} required/>
                 <input className="inp" type="number" onChange={handleInput} placeholder="Yaş Girin" name="age" value={userDetails.age} required/>
 
                 <button className="btn">Kayıt Ol</button>
