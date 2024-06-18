b084ac17b97213b89055c90cfb934c5918986dc3
Serhat Yildiz <yildizserhat@yahoo.com>
2024-06-18 14:42:13 +0300

    urls added

--- client/src/component/FoodData.jsx
+++ client/src/component/FoodData.jsx
@@ -56,7 +56,7 @@ export default function FoodData(props) {
     }
 
     function createFoodItem(trackedItem) {
-        fetch("http://localhost:8000/track", {
+        fetch("https://galwinapp-7861c5aaed27.herokuapp.com/track", {
             method: "POST",
             body: JSON.stringify(trackedItem),
             headers: {
@@ -86,7 +86,7 @@ export default function FoodData(props) {
 
     function updateFoodItem(trackedItem) {
         console.log(`Updating food tracking record with ID: ${trackedItem.id}`);
-        fetch(`http://localhost:8000/track/${trackedItem.id}`, {
+        fetch(`https://galwinapp-7861c5aaed27.herokuapp.com/track/${trackedItem.id}`, {
             method: "PUT",
             body: JSON.stringify(trackedItem),
             headers: {
